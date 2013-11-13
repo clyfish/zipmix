ce563b5a22176c1e6cc583dbeb08b17666acad6c
Cheng Liangyu <clyfish@gmail.com>
2013-11-13 20:09:55 +0800

    modify apkopt.sh to exclude by file extensions

--- apkopt.sh
+++ apkopt.sh
@@ -10,7 +10,12 @@ tmpdir=`mktemp -d`
 cd "$tmpdir"
 mkdir tmp
 cd tmp
-unzip "$zip" -x res/raw/*
+unzip "$zip" -x "*.jpg" "*.jpeg" "*.png" "*.gif" \
+                "*.wav" "*.mp2" "*.mp3" "*.ogg" "*.aac" \
+                "*.mpg" "*.mpeg" "*.mid" "*.midi" "*.smf" "*.jet" \
+                "*.rtttl" "*.imy" "*.xmf" "*.mp4" "*.m4a" \
+                "*.m4v" "*.3gp" "*.3gpp" "*.3g2" "*.3gpp2" \
+                "*.amr" "*.awb" "*.wma" "*.wmv"
 $BIN/kzip -r ../1.zip *
 cd ..
 $BIN/zipmix.py "$zip" 1.zip 2.zip
